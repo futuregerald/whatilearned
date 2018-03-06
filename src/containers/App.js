1f8f1df54e3129040ad3954dbe2908647b46855e
Gerald O <gerald.onyango@gmail.com>
2018-03-05 21:49:57 -0500

    added styled componenets

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,104 +1,93 @@
 import React, { Component } from 'react';
-import '../css/app.css';
-import 'typeface-roboto'
-import {TextField, Card, CardContent, AppBar, Toolbar, Typography} from 'material-ui'
+import 'typeface-roboto';
+import { TextField, Card, CardContent, AppBar, Toolbar, Typography } from 'material-ui';
+import styled from 'styled-components';
 
-const styles = {
-  root: {
-    flexGrow: 1,
-  },
-  flex: {
-    flex: 1,
-  },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20,
-  },
-};
+const BodyContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-gap: 20px;
+  margin-top: 10px;
+  @media (min-width: 1100px) {
+    grid-template-columns: 1fr 3fr 1fr;
+  }
+`;
+
+const CenterContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-gap: 20px;
+  justify-items: center;
+  @media (max-width: 700px) {
+    grid-template-columns: 1fr;
+  }
+`;
+
+const StyledCard = styled(Card)`
+  display: grid;
+  grid-template-columns: 0.9fr;
+  width: 100%;
+`;
+const StyledCardContent = styled(CardContent)`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  width: 100%;
+  grid-gap: 20px;
+  @media (max-width: 700px) {
+    grid-template-columns: 1fr;
+  }
+`;
 
 class App extends Component {
-  
+  constructor() {
+    super();
+    this.state = {
+      loggedIn: false,
+    };
+  }
   render() {
     return (
       <div>
-      <AppBar position="static" color="default">
-      <Toolbar>
-        <Typography variant="title" color="inherit">
-          What I Learned, Today
-        </Typography>
-      </Toolbar>
-    </AppBar>
-      <div className="container">
-      <div></div>
-      <div className="center-container">
-      <Card
-      className="card-cell"
-      >
-        <CardContent
-        className="card-cell-content"
-        >
-        <TextField
-          id="1"
-          label="I learned..."
-          helperText="Example: CSS grid"
-          fullWidth
-        /> 
-      
-        <TextField
-        fullWidth
-        id="1"
-        label="From..."
-        helperText="Example: Wes Bos"
-        /> 
-        </CardContent>
-      </Card>
-      <Card
-      className="card-cell"
-      >
-        <CardContent
-        className="card-cell-content"
-        >
-        <TextField
-          id="1"
-          label="I learned..."
-          helperText="Example: React"
-          fullWidth
-        /> 
-        <TextField
-        fullWidth
-        id="1"
-        label="From..."
-        helperText="Example:  https://www.udemy.com/react-redux/"
-        /> 
-        </CardContent>
-      </Card>
-      <Card
-      className="card-cell"
-      >
-        <CardContent
-        className="card-cell-content"
-        >
-        <TextField
-          id="1"
-          label="I learned..."
-          helperText="Example: Node"
-          fullWidth
-        /> 
-        <TextField
-        fullWidth
-        id="1"
-        label="From..."
-        helperText="Example: https://medium.com/the-node-js-collection/tagged/tutorial"
-        /> 
-        </CardContent>
-      </Card>
-      </div>
-      <div></div>
-      </div>
+        <AppBar position="static" color="default">
+          <Toolbar>
+            <Typography variant="title" color="inherit">
+              What I Learned, Today
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <BodyContainer>
+          <div />
+          <CenterContainer>
+            <StyledCard>
+              <StyledCardContent>
+                <TextField id="1" label="I learned..." helperText="Example: CSS grid" fullWidth />
+
+                <TextField fullWidth id="1" label="From..." helperText="Example: Wes Bos" />
+              </StyledCardContent>
+            </StyledCard>
+            <StyledCard>
+              <StyledCardContent>
+                <TextField id="1" label="I learned..." helperText="Example: React" fullWidth />
+                <TextField fullWidth id="1" label="From..." helperText="Example:  https://www.udemy.com/react-redux/" />
+              </StyledCardContent>
+            </StyledCard>
+            <StyledCard>
+              <StyledCardContent>
+                <TextField id="1" label="I learned..." helperText="Example: Node" fullWidth />
+                <TextField
+                  fullWidth
+                  id="1"
+                  label="From..."
+                  helperText="Example: https://medium.com/the-node-js-collection/tagged/tutorial"
+                />
+              </StyledCardContent>
+            </StyledCard>
+          </CenterContainer>
+          <div />
+        </BodyContainer>
       </div>
     );
   }
 }
 
 export default App;
-
